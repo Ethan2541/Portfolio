939c245cb0f08adbd13b8363834ad5e1ca7c66b6
Malek Bouzarkouna <MalekBouzarkouna58@gmail.com>
2024-12-16 20:51:23 +0100

    fix the sitemap with the path

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,13 +1,9 @@
-// next-sitemap.js
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.malekbouzarkouna.com',
-  generateRobotsTxt: true, // Génère aussi un robots.txt
-  changefreq: 'monthly',
-  priority: 0.7,
-  robotsTxtOptions: {
-    policies: [
-      { userAgent: '*', allow: '/' },
-    ],
-  },
+  siteUrl: 'https://www.malekbouzarkouna.com', // Ton URL principale
+  generateRobotsTxt: true, // Générer robots.txt
+  exclude: ['/api/*'], // Exclut les API ou autres routes inutiles
+  generateIndexSitemap: false, // Empêche la génération de `sitemap-index`
+  changefreq: 'monthly', // Fréquence des mises à jour
+  priority: 0.7, // Priorité par défaut
 };
-
